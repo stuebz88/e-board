e45641e6460073b77a23c2cc6d38a9156fd5df4c
Zack Stuebs <stuebz88@uwosh.edu>
2017-11-28 12:50:29 -0600

    Write and read to database, partial admin front-end

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,32 @@
 var express = require('express');
+var async = require('async');
+var mongoose = require('mongoose');
+var Emp = require('../models/emp');
 var router = express.Router();
 
 router.get('/',function(req,res,next) {
-    res.render('admin');
+    Emp.find(function(err,result) {
+        res.render('admin',{emps: result});
+    });
+    /*
+    async.series([function(callback) {
+        Emp.find(function(err,emps) {
+            callback(err,emps)
+        });
+    }], function(err,results) {
+        res.render('admin',{emps: results});
+    });*/
 });
+
+function add() {
+    var test = new Emp({url:'http://www.schedulesource.net/Enterprise/Public/EmployeeSchedule.aspx?&id=C6E25BA5-197A-4787-855B-F3A5AB65D391&format=ical',name:'Vaj, Ntxawm',nickname:'Zer'});
+    test.save(function(err,result) {
+        if(err)
+        {
+            return console.log(err);
+        }
+        console.log(test.name);
+    });
+}
 
 module.exports = router;
